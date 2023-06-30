fa26ae8752365417e2c52a0e13cb019e650402d4
meryemipekci <meryemdelgurgen@hotmail.com>
2023-06-30 13:14:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bs-portfolio-web-page-with-html-css-bs-and-js
---
+++ cdn. codes/owl.js
@@ -0,0 +1,3 @@
+c:\Users\merye\AppData\Local\Temp\_PA94\OwlCarousel2-2.3.4\dist\assets\owl.carousel.min.css
+
+c:\Users\merye\AppData\Local\Temp\_PA670\OwlCarousel2-2.3.4\dist\assets\owl.theme.default.min.css
